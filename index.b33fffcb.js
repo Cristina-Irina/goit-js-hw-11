4c889b4b7b3f9d5543c4653df75bbf578444d4f3
Cristina-Irina <121606106+Cristina-Irina@users.noreply.github.com>
2023-07-08 10:47:21 +0000

    Deploying to gh-pages from @ Cristina-Irina/goit-js-hw-11@f49f4839dacf3ee01e9fe8bc7e32082a77854747 🚀

---
+++ index.b33fffcb.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){var e=document.querySelector(".header"),r=e.offsetHeight;window.scrollY>r?e.classList.add("header--no-transparency"):e.classList.remove("header--no-transparency")};
+//# sourceMappingURL=index.b33fffcb.js.map
